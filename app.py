2975db6db3d59191743a7495d45629c61c88215d
DanielChu <danielchu@danielchus-mbp.sg.ibm.com>
2019-04-15 16:46:01 +0800

    First commit

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__) 
 @app.route("/") 
 def hello(): 
-	return "Hello Beautiful World!" 
+	return "This is a Hello Beautiful World! moxing9876" 
 if __name__ == "__main__": 
 	app.run(host="0.0.0.0")
